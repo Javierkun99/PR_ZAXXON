bf810649124c9b669c29097c0e32c72bab51d49c
Javier Gómez Sánchez <jagosakun27@gmail.com>
2022-02-16 19:23:09 +0100

    Commit música acumulada
    He metido música en cada escena menos la intro y la de la configuración (esta es la más importante)

--- PR_ZAXXON_GomezJavier/Assets/Scripts/UI/ClasificaciónOpen.cs
+++ PR_ZAXXON_GomezJavier/Assets/Scripts/UI/ClasificaciónOpen.cs
@@ -5,17 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Clasificación : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
     public void Clasi()
     {
         SceneManager.LoadScene(4);
